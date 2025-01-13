19ee74e1c86f4de2d7e7b50ec6f60c27f871493b
malligirisfdc505 <malligirisfdc505@gmail.com>
2025-01-13 14:13:36 +0530

    teeeeeeeeee

---
+++ force-app/main/default/aura/owncalculator/owncalculatorHelper.js
@@ -0,0 +1,10 @@
+({
+    callMe : function(component) {
+        var Firstnumber=component.find("fnum").get("v.value");
+        var Secondnumber=component.find("snum").get("v.value");
+        console.log(Firstnumber);
+        console.log(Secondnumber);
+        
+        
+    }
+})
